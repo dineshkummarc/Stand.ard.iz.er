d94f034b90f34a0072500fb9c675c51af9b1b330
Mark McDonnell <mark@stormcreative.co.uk>
2011-05-03 09:56:43 +0100

    un-minify emilejs library code

--- Standardizer.js
+++ Standardizer.js
@@ -54,6 +54,8 @@
 					 fallback_onload, 
 					 explorerTimer,
 					 readyStateTimer,
+					 // Had to duplicate isIE function from above as both sections of the script rely on isIE being an IIFE 
+					 // (immediately invoked function expression)
 					 isIE = (function() {
 						var undef,
 							 v = 3,
@@ -453,7 +455,7 @@
 					e = __standardizer.events.standardize(e) || __standardizer.events.standardize(window.event);
 					
 					// Get a reference to the hash table of event handlers
-					var handlers = this.events[e.type];
+					handlers = this.events[e.type];
 
 					// Execute each event handler
 					for (var i in handlers) {
@@ -858,35 +860,75 @@
 			 */
 		 	animation: (function() {
 				var parseEl = document.createElement('div'),
-				 props = ('backgroundColor borderBottomColor borderBottomWidth borderLeftColor borderLeftWidth '+
-				 'borderRightColor borderRightWidth borderSpacing borderTopColor borderTopWidth bottom color fontSize '+
-				 'fontWeight height left letterSpacing lineHeight marginBottom marginLeft marginRight marginTop maxHeight '+
-				 'maxWidth minHeight minWidth opacity outlineColor outlineOffset outlineWidth paddingBottom paddingLeft '+
-				 'paddingRight paddingTop right textIndent top width wordSpacing zIndex').split(' ');
+					 props = ('backgroundColor borderBottomColor borderBottomWidth borderLeftColor borderLeftWidth '+
+				 				 'borderRightColor borderRightWidth borderSpacing borderTopColor borderTopWidth bottom color fontSize '+
+				 				 'fontWeight height left letterSpacing lineHeight marginBottom marginLeft marginRight marginTop maxHeight '+
+				 				 'maxWidth minHeight minWidth opacity outlineColor outlineOffset outlineWidth paddingBottom paddingLeft '+
+				 				 'paddingRight paddingTop right textIndent top width wordSpacing zIndex').split(' ');
 				
-				function interpolate(source,target,pos){ return (source+(target-source)*pos).toFixed(3); }
-				function s(str, p, c){ return str.substr(p,c||1); }
-				function color(source,target,pos){
-				 var i = 2, j, c, tmp, v = [], r = [];
-				 while(j=3,c=arguments[i-1],i--)
-				   if(s(c,0)=='r') { c = c.match(/\d+/g); while(j--) v.push(~~c[j]); } else {
-				     if(c.length==4) c='#'+s(c,1)+s(c,1)+s(c,2)+s(c,2)+s(c,3)+s(c,3);
-				     while(j--) v.push(parseInt(s(c,1+j*2,2), 16)); }
-				 while(j--) { tmp = ~~(v[j+3]+(v[j]-v[j+3])*pos); r.push(tmp<0?0:tmp>255?255:tmp); }
-				 return 'rgb('+r.join(',')+')';
+				function interpolate(source,target,pos) { 
+					return (source+(target-source)*pos).toFixed(3); 
 				}
 				
-				function parse(prop){
-				 var p = parseFloat(prop), q = prop.replace(/^[\-\d\.]+/,'');
-				 return isNaN(p) ? { v: q, f: color, u: ''} : { v: p, f: interpolate, u: q };
+				function s(str, p, c) { 
+					return str.substr(p,c||1);
 				}
 				
-				function normalize(style){
-				 var css, rules = {}, i = props.length, v;
-				 parseEl.innerHTML = '<div style="'+style+'"></div>';
-				 css = parseEl.childNodes[0].style;
-				 while(i--) if(v = css[props[i]]) rules[props[i]] = parse(v);
-				 return rules;
+				function color(source,target,pos) {
+					var i = 2, 
+						 j, 
+						 c, 
+						 tmp, 
+						 v = [], 
+						 r = [];
+					
+					while (j=3,c=arguments[i-1],i--) {					
+						if (s(c,0)=='r') { 
+							c = c.match(/\d+/g); while(j--) v.push(~~c[j]);
+						}
+						else {
+							if(c.length==4) {
+								c='#'+s(c,1)+s(c,1)+s(c,2)+s(c,2)+s(c,3)+s(c,3);
+							}
+							while (j--) {
+								v.push(parseInt(s(c,1+j*2,2), 16)); 
+							}
+						}
+					}
+					
+					while (j--) { 
+						tmp = ~~(v[j+3]+(v[j]-v[j+3])*pos); r.push(tmp<0?0:tmp>255?255:tmp); 
+					}
+					
+					return 'rgb('+r.join(',')+')';
+				}
+				
+				function parse(prop) {
+					var p = parseFloat(prop), 
+						 q = prop.replace(/^[\-\d\.]+/,'');
+						 
+					return isNaN(p) ? { v: q, f: color, u: ''} : { v: p, f: interpolate, u: q };
+				}
+				
+				function normalize(style) {
+					var css, 
+						 rules = {}, 
+						 i = props.length, 
+						 v;
+						 
+					parseEl.innerHTML = '<div style="'+style+'"></div>';
+					
+					css = parseEl.childNodes[0].style;
+					
+					while(i--) {
+						
+						if(v = css[props[i]]) {
+							rules[props[i]] = parse(v);
+							console.log('rules[props[i]]', rules[props[i]]);
+						}
+					}
+					
+					return rules;
 				}
 				
 				function bounce(pos) {
@@ -904,18 +946,43 @@
 				return function(el, style, opts, after) {
 					el = typeof el == 'string' ? document.getElementById(el) : el;
 					opts = opts || {};
-					var target = normalize(style), comp = el.currentStyle ? el.currentStyle : getComputedStyle(el, null),
-					  prop, current = {}, start = +new Date, dur = opts.duration||200, finish = start+dur, interval,
-					  easing = opts.easing || function(pos){ return (-Math.cos(pos*Math.PI)/2) + 0.5; };
-					  // Modification made to include 'bounce' easing
-					  if (easing === 'bounce') { easing = bounce; }
-					for(prop in target) current[prop] = parse(comp[prop]);
-					interval = setInterval(function(){
-					  var time = +new Date, pos = time>finish ? 1 : (time-start)/dur;
-					  for(prop in target)
-					    el.style[prop] = target[prop].f(current[prop].v,target[prop].v,easing(pos)) + target[prop].u;
-					  if(time>finish) { clearInterval(interval); opts.after && opts.after(); after && setTimeout(after,1); }
-					},10);
+					
+					var target = normalize(style), 
+						 comp = el.currentStyle ? el.currentStyle : getComputedStyle(el, null),
+						 prop, 
+						 current = {}, 
+						 start = +new Date, 
+						 dur = opts.duration||200, 
+						 finish = start+dur, 
+						 interval,
+						 easing = opts.easing || function(pos) { 
+						 	return (-Math.cos(pos*Math.PI)/2) + 0.5;
+						 };
+						 
+					// Modification made to include 'bounce' easing
+					if (easing === 'bounce') { 
+						easing = bounce;
+					}
+					
+					for (prop in target) {
+						current[prop] = parse(comp[prop]);
+					}
+					
+					interval = setInterval(function() {
+						var time = +new Date, pos = time>finish ? 1 : (time-start)/dur;
+					  
+						for(prop in target) {
+							el.style[prop] = target[prop].f(current[prop].v,
+							target[prop].v,
+							easing(pos)) + target[prop].u;
+						}
+					
+						if(time > finish) { 
+							clearInterval(interval); 
+							opts.after && opts.after(); 
+							after && setTimeout(after, 1);
+						}
+					}, 10);
 				};
 			}()),
 			
