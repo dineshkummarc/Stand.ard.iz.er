b993d7cbc62e7d64c04f48066a18b565340a547c
Mark McDonnell <mark.mcdx@gmail.com>
2011-08-10 09:34:22 +0100

    Updated __standardizer.utilities.getType method
    - Original method wasn't able to check for Null or Undefined or Global
    - So moved to use @cowboy's version: https://gist.github.com/1131946
    - Had to update the inline js code within the index.html file

--- Standardizer.js
+++ Standardizer.js
@@ -947,25 +947,36 @@
 				/**
 				 * Following property stores a reference to the core Object's toString() method.
 				 * This allows us to access JavaScript's internal [[Class]] property which helps us determine the type of certain primitives.
+				 * This version was updated to use @cowboy's version: https://gist.github.com/1131946
 				 * 
 				 * Example Usage:
-				 * 	__standardizer.utilities.getType([1, 2, 3]); 					// [object Array]
-				 * 	__standardizer.utilities.getType({ a: 1, b: 2, c: 3 }); 		// [object Object]
-				 * 	__standardizer.utilities.getType(123); 							// [object Number]
-				 * 	__standardizer.utilities.getType(new Date); 						// [object Date]
-				 * 	__standardizer.utilities.getType("String"); 						// [object String]
+				 * 	__standardizer.utilities.getType([1, 2, 3]); 					// Array
+				 * 	__standardizer.utilities.getType({ a: 1, b: 2, c: 3 }); 		// Object
+				 * 	__standardizer.utilities.getType(123); 							// Number
+				 * 	__standardizer.utilities.getType(new Date); 						// Date
+				 * 	__standardizer.utilities.getType("String"); 						// String
+				 * 	__standardizer.utilities.getType(window); 						// Global
 				 * 
-				 * This doesn't work with null or undefined, because executing call() with null or undefined passes window as this.
-				 * This code is only useful for checking 'native' objects.
+				 * Running {}.toString.call() doesn't work with null or undefined, because executing call() with null or undefined passes window as this.
+				 * So for Null we check explicit value/type match, and after that if Null doesn't match then we check value is null so we know the obj is Undefined.
 				 * 
-				 * @return { String } the internal [[Class]] value for the specified object (i.e. the object's true data type)
+				 * @return { String } a trimmed version of the internal [[Class]] value for the specified object (i.e. the object's true data type)
 				 */
 				getType: (function() {
 					
-					var internal = {}.toString;
+					var types = {};
 					
-					return function(type) {
-						return internal.call(type);
+					return function(obj) {
+						var key;
+						
+						// If the object is null, return "Null" (IE <= 8)
+						return obj === null ? "Null"
+							// If the object is undefined, return "Undefined" (IE <= 8)
+							: obj == null ? "Undefined"
+							// If the object is the global object, return "Global"
+							: obj === window ? "Global"
+							// Otherwise return the XXXXX part of the full [object XXXXX] value, from cache if possible.
+							: types[key = types.toString.call(obj)] || (types[key] = key.slice(8, -1));
 					};
 					
 				}()),
